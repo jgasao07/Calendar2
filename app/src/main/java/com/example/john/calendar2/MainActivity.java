a5e763e68bd2a2712fcd11a268732f729f8d3e23
jgasao07 <gasao@wisc.edu>
2016-03-10 17:18:49 -0600

    Initial commit

---
+++ app/src/main/java/com/example/john/calendar2/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.john.calendar2;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    CalendarView calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendar = (CalendarView) findViewById(R.id.calendar);
+        calendar.setOnDateChangeListener(new OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view,
+                                            int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(),
+                        dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+}
