4808971d904786dc35cef1017670ff309e90fef9
John Gasao <gasao@wisc.edu>
2016-03-10 22:53:58 -0600

    Views good, need to trace through

--- app/src/main/java/com/example/john/calendar2/MainActivity.java
+++ app/src/main/java/com/example/john/calendar2/MainActivity.java
@@ -1,52 +1,121 @@
 package com.example.john.calendar2;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
+import java.util.GregorianCalendar;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.CalendarView;
-import android.widget.CalendarView.OnDateChangeListener;
-import android.widget.Toast;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import com.example.john.calendar2.CalendarAdapter;
+import com.example.john.calendar2.CalendarCollection;
 
-public class MainActivity extends ActionBarActivity {
-
-    CalendarView calendar;
+public class MainActivity extends Activity {
+    public GregorianCalendar cal_month, cal_month_copy;
+    private CalendarAdapter cal_adapter;
+    private TextView tv_month;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        calendar = (CalendarView) findViewById(R.id.calendar);
-        calendar.setOnDateChangeListener(new OnDateChangeListener() {
+
+        cal_month = (GregorianCalendar) GregorianCalendar.getInstance();
+        cal_month_copy = (GregorianCalendar) cal_month.clone();
+        cal_adapter = new CalendarAdapter(this, cal_month,CalendarCollection.date_collection_arr);
+
+
+
+        tv_month = (TextView) findViewById(R.id.tv_month);
+        tv_month.setText(android.text.format.DateFormat.format("MMMM yyyy", cal_month));
+
+        ImageButton previous = (ImageButton) findViewById(R.id.ib_prev);
+
+        previous.setOnClickListener(new OnClickListener() {
 
             @Override
-            public void onSelectedDayChange(CalendarView view,
-                                            int year, int month, int dayOfMonth) {
-                Toast.makeText(getApplicationContext(),
-                        dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                setPreviousMonth();
+                refreshCalendar();
             }
         });
+
+        ImageButton next = (ImageButton) findViewById(R.id.Ib_next);
+        next.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setNextMonth();
+                refreshCalendar();
+
+            }
+        });
+
+        GridView gridview = (GridView) findViewById(R.id.gv_calendar);
+        gridview.setAdapter(cal_adapter);
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                ((CalendarAdapter) parent.getAdapter()).setSelected(v,position);
+                String selectedGridDate = CalendarAdapter.day_string
+                        .get(position);
+
+                String[] separatedTime = selectedGridDate.split("-");
+                String gridvalueString = separatedTime[2].replaceFirst("^0*","");
+                int gridvalue = Integer.parseInt(gridvalueString);
+
+                if ((gridvalue > 10) && (position < 8)) {
+                    setPreviousMonth();
+                    refreshCalendar();
+                } else if ((gridvalue < 7) && (position > 28)) {
+                    setNextMonth();
+                    refreshCalendar();
+                }
+                ((CalendarAdapter) parent.getAdapter()).setSelected(v,position);
+
+                ((CalendarAdapter) parent.getAdapter()).getPositionList(selectedGridDate, MainActivity.this);
+            }
+
+        });
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    protected void setNextMonth() {
+        if (cal_month.get(GregorianCalendar.MONTH) == cal_month
+                .getActualMaximum(GregorianCalendar.MONTH)) {
+            cal_month.set((cal_month.get(GregorianCalendar.YEAR) + 1),
+                    cal_month.getActualMinimum(GregorianCalendar.MONTH), 1);
+        } else {
+            cal_month.set(GregorianCalendar.MONTH,
+                    cal_month.get(GregorianCalendar.MONTH) + 1);
         }
-        return super.onOptionsItemSelected(item);
+
     }
-}
+
+    protected void setPreviousMonth() {
+        if (cal_month.get(GregorianCalendar.MONTH) == cal_month
+                .getActualMinimum(GregorianCalendar.MONTH)) {
+            cal_month.set((cal_month.get(GregorianCalendar.YEAR) - 1),
+                    cal_month.getActualMaximum(GregorianCalendar.MONTH), 1);
+        } else {
+            cal_month.set(GregorianCalendar.MONTH,
+                    cal_month.get(GregorianCalendar.MONTH) - 1);
+        }
+
+    }
+
+    public void refreshCalendar() {
+        cal_adapter.refreshDays();
+        cal_adapter.notifyDataSetChanged();
+        tv_month.setText(android.text.format.DateFormat.format("MMMM yyyy", cal_month));
+    }
+
 }
